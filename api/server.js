2957b3163cc4c7873780d41315d99c3e4782e0c0
Yash patil <149749700+PATILYASHH@users.noreply.github.com>
2024-11-15 10:53:38 +0000

    vercel deploy

--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
@@ -11,23 +10,24 @@ app.use(express.static('public'));
 let users = 0;
 
 io.on('connection', (socket) => {
-    users++;
-    io.emit('user connected', users);
+  users++;
+  io.emit('user connected', users);
 
-    socket.on('chat message', (data) => {
-        const { msg, username } = data;
-        const timestamp = new Date().toLocaleTimeString();
-        io.emit('chat message', { msg, username, timestamp });
-    });
+  socket.on('chat message', (data) => {
+    const { msg, username } = data;
+    const timestamp = new Date().toLocaleTimeString();
+    io.emit('chat message', { msg, username, timestamp });
+  });
 
-    socket.on('typing', (username) => {
-        socket.broadcast.emit('typing', username);
-    });
+  socket.on('typing', (username) => {
+    socket.broadcast.emit('typing', username);
+  });
 
-    socket.on('disconnect', () => {
-        users--;
-        io.emit('user disconnected', users);
-    });
+  socket.on('disconnect', () => {
+    users--;
+    io.emit('user disconnected', users);
+  });
 });
 
+// Export the server (needed for Vercel serverless functions)
 module.exports = server;
