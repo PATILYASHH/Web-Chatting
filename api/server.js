7794674f7054a82d935e31c962522bc1ee51e950
Yash patil <149749700+PATILYASHH@users.noreply.github.com>
2024-11-15 16:12:26 +0530

    zc

---
+++ api/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+let users = 0;
+
+io.on('connection', (socket) => {
+    users++;
+    io.emit('user connected', users);
+
+    socket.on('chat message', (data) => {
+        const { msg, username } = data;
+        const timestamp = new Date().toLocaleTimeString();
+        io.emit('chat message', { msg, username, timestamp });
+    });
+
+    socket.on('typing', (username) => {
+        socket.broadcast.emit('typing', username);
+    });
+
+    socket.on('disconnect', () => {
+        users--;
+        io.emit('user disconnected', users);
+    });
+});
+
+module.exports = server;
